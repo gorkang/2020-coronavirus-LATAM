10a90bd8da5ff77b9bcad5c4d0156d366cd58b06
Gorka Navarrete <gorkang@gmail.com>
2020-04-27 12:38:59 +0100

    delete old code

--- combined_plots.R
+++ combined_plots.R
@@ -6,13 +6,11 @@ library(cowplot)
 library(dplyr)
 library(ggplot2)
 library(ggrepel)
+library(here)
 library(httr)
 library(purrr)
 library(readr)
 library(scales)
-# library(shiny)
-# library(shinyjs)
-# library(shinythemes)
 library(tidyr)
 library(vroom)
 
@@ -22,10 +20,7 @@ library(vroom)
 
 cases_deaths = "deaths" #cases deaths
 
-source(here::here("R/download_or_load.R"))
 source(here::here("R/download_or_load_JH_API.R"))
-
-source(here::here("R/fetch_worldometers_safely.R"))
 source(here::here("R/data-download.R"))
 source(here::here("R/data-preparation.R"))
 
@@ -34,36 +29,18 @@ data_download()
 
 
 source("R/LATAM_plot1.R")
+p1_to_save
 source("R/LATAM_plot2.R")
+# p2_to_save
 
-
-# title = paste0("Coronavirus confirmed ", cases_deaths , if (relative == TRUE) " / million people"),
-# subtitle = paste0("Starting at ",  "10" ," or more accumulated ", cases_deaths),
-
-# title <- cowplot::ggdraw() + 
-#   draw_label(
-#     "Coronavirus confirmed deaths",
-#     fontface = 'bold',
-#     x = 0,
-#     hjust = 0
-#   ) +
-#   theme(
-#     # add margin on the left of the drawing canvas, so title is aligned with left edge of first plot
-#     plot.margin = margin(0, 0, 0, 7)
-#   )
 
 combined_plot = 
   cowplot::plot_grid(
-    # title,
     p1_to_save,
     p2_to_save,
     nrow = 2,
     labels = c("A)", "B)"),
     rel_heights = c(1, 1)
-    # nrow = 3,
-    # labels = c("", "A)", "B)"),
-    # rel_heights = c(0.1, 1, 1)
-    
   )
 
 combined_plot
